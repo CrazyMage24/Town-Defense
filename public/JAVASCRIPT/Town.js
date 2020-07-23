e4184257c82e280350de3f31bd1b55cccbf54e3d
CrazyMage24 <gaborszentes9@gmail.com>
2020-07-23 17:55:02 +0200

    Igazabol mukodik, fullosan jo az egesz

--- public/JAVASCRIPT/Town.js
+++ public/JAVASCRIPT/Town.js
@@ -2,6 +2,10 @@ class Town
 {
 	constructor(name,fonts)
 	{
+		this.selected = null;
+		this.highlight = null;
+		this.battle = null;
+		
 		this.fonts = fonts;
 		this.inventory = 
 		[
@@ -19,55 +23,13 @@ class Town
 			{name: "Max Builders", key: 10},	//[11]
 			{name: "Free man", key: 2},			//[12]
 			
-			{name: "Food", key: 50},			//[13]
-			{name: "Max Food", key: 100},		//[14]
-			{name: "Resource", key: 40},		//[15]
-			{name: "Max Resource", key: 100},	//[16]
+			{name: "Food", key: 12},			//[13]
+			{name: "Max Food", key: 50},		//[14]
+			{name: "Resource", key: 20},		//[15]
+			{name: "Max Resource", key: 50},	//[16]
 			{name: "Happiness", key: 80},		//[17]
 			{name: "Max Happiness", key: 100},	//[18]
 		];
-		
-		this.setWidgets();
-	}
-	
-	run()
-	{
-		if(this.battle != null)
-		{
-			this.battle.run();
-		}
-		else
-		{
-			for(i = 0; i < this.widgets.length; i++)
-			{
-				this.widgets[i].show();
-			}
-		}
-		
-	}
-	
-	setWidgets()
-	{
-		this.selected = null;
-		this.highlight = null;
-		this.battle = null;
-		
-		this.widgets = [];
-		this.buildings = [];
-		this.widgets.push(new AnimatedSign(1300,325,300,100,this.inventory[0].key,60,"PICS/wooden_sign2.png",this.fonts[1]));
-		
-		this.widgets.push(new FillBar(50,50,200,20,"Population",this.inventory[2].key,this.inventory[3].key,false,this.fonts[0]));
-		this.widgets.push(new FillBar(300,50,200,20,"Soldiers",this.inventory[4].key,this.inventory[5].key,true,this.fonts[0]));
-		this.widgets.push(new FillBar(550,50,200,20,"Farmers",this.inventory[6].key,this.inventory[7].key,true,this.fonts[0]));
-		this.widgets.push(new FillBar(800,50,200,20,"Workers",this.inventory[8].key,this.inventory[9].key,true,this.fonts[0]));
-		this.widgets.push(new FillBar(1050,50,200,20,"Builders",this.inventory[10].key,this.inventory[11].key,true,this.fonts[0]));
-		this.widgets.push(new FillBar(1300,50,200,20,"Free man",this.inventory[12].key,this.inventory[3].key,false,this.fonts[0]));
-		
-		this.widgets.push(new FillBar(450,150,200,20,"Food",this.inventory[13].key,this.inventory[14].key,false,this.fonts[0]));
-		this.widgets.push(new FillBar(700,150,200,20,"Resource",this.inventory[15].key,this.inventory[16].key,false,this.fonts[0]));
-		this.widgets.push(new FillBar(950,150,200,20,"Happiness",this.inventory[17].key,this.inventory[18].key,false,this.fonts[0]));
-		
-		this.widgets.push(new NextDay(50,200,150,100,"Next Day",this,this.fonts[0]));
 		
 		this.population_list = [];
 		for(i = 0; i < this.inventory[2].key; i++)
@@ -94,14 +56,96 @@ class Town
 			}
 			this.population_list.push(person);
 		}
-		this.widgets.push(new List(50,350,300,400,"",this.population_list,this.fonts[0]));
+		this.defaultWidgets();
+	}
+	
+	run()
+	{
+		if(this.battle != null)
+		{
+			this.battle.run();
+		}
+		else
+		{
+			for(i = 0; i < this.widgets.length; i++)
+			{
+				this.widgets[i].show();
+			}
+		}
+		
+	}
+	
+	setPeople()
+	{
+		var soldiers = this.inventory[4].key;
+		var farmers = this.inventory[6].key;
+		var workers = this.inventory[8].key;
+		var builders = this.inventory[10].key;
+		var freeman = this.inventory[12].key;
+		var person = null;
+		
+		this.population_list = [];
+		for(var i = 0; i < this.inventory[2].key; i++)
+		{
+			if(soldiers > 0)
+			{
+				person = new Person("Person" + (i+1), "Soldier",80,false,false);
+				soldiers--;
+			}
+			else if(farmers > 0)
+			{
+				person = new Person("Person" + (i+1), "Farmer",80,false,false);
+				farmers--;
+			}
+			else if(workers > 0)
+			{
+				person = new Person("Person" + (i+1), "Worker",80,false,false);
+				workers--;
+			}
+			else if(builders > 0)
+			{
+				person = new Person("Person" + (i+1), "Builder",80,false,false);
+				builders--;
+			}
+			else if(freeman > 0)
+			{
+				person = new Person("Person" + (i+1), "Free man",80,false,false);
+				freeman--;
+			}
+			this.population_list.push(person);
+		}
+	}
+	
+	defaultWidgets()
+	{	
+		this.widgets = [];
+		this.buildings = [];
+		
+		/* [0] */ this.widgets.push(new FillBar(50,50,200,20,"Population",this.inventory[2].key,this.inventory[3].key,false,this.fonts[0]));
+		/* [1] */ this.widgets.push(new FillBar(300,50,200,20,"Soldiers",this.inventory[4].key,this.inventory[5].key,true,this.fonts[0]));
+		/* [2] */ this.widgets.push(new FillBar(550,50,200,20,"Farmers",this.inventory[6].key,this.inventory[7].key,true,this.fonts[0]));
+		/* [3] */ this.widgets.push(new FillBar(800,50,200,20,"Workers",this.inventory[8].key,this.inventory[9].key,true,this.fonts[0]));
+		/* [4] */ this.widgets.push(new FillBar(1050,50,200,20,"Builders",this.inventory[10].key,this.inventory[11].key,true,this.fonts[0]));
+		/* [5] */ this.widgets.push(new FillBar(1300,50,200,20,"Free man",this.inventory[12].key,this.inventory[3].key,false,this.fonts[0]));
+		
+		/* [6] */ this.widgets.push(new FillBar(450,150,200,20,"Food",this.inventory[13].key,this.inventory[14].key,false,this.fonts[0]));
+		/* [7] */ this.widgets.push(new FillBar(700,150,200,20,"Resource",this.inventory[15].key,this.inventory[16].key,false,this.fonts[0]));
+		/* [8] */ this.widgets.push(new FillBar(950,150,200,20,"Happiness",this.inventory[17].key,this.inventory[18].key,false,this.fonts[0]));
+		
+		/* [9] */ this.widgets.push(new NextDay(50,200,150,100,"Next Day",this,this.fonts[0]));
+		/* [10] */ this.widgets.push(new List(50,350,300,400,"",this.population_list,this.fonts[0]));
 	}
 	
 	newDay(inventory)
 	{
 		loadingScreen();
+		this.selected = null;
+		this.highlight = null;
+		this.battle = null;
+		
 		this.inventory = inventory;
-		this.setWidgets();
+		this.setPeople();
+		this.defaultWidgets();
 	}
 	
 	mouseMoved(mouseX,mouseY)
@@ -188,9 +232,50 @@ class Town
 	
 	checkFreeman(z)
 	{
-		this.inventory[z].key = this.selected.changeValue(keyCode,this.widgets[6].value);
+		this.inventory[z].key = this.selected.changeValue(keyCode,this.widgets[5].value);
 		this.inventory[12].key = this.inventory[2].key - this.inventory[4].key - this.inventory[6].key - this.inventory[8].key - this.inventory[10].key;
-		this.widgets[6].value = this.inventory[12].key;
+		this.widgets[5].value = this.inventory[12].key;
+		this.checkOccupation();
 	}
 	
+	checkOccupation()
+	{
+		var soldiers = this.widgets[1].value;
+		var farmers = this.widgets[2].value;
+		var workers = this.widgets[3].value;
+		var builders = this.widgets[4].value;
+		var freeman = this.widgets[5].value;
+		var person = null;
+		this.population_list = [];
+		for(var i = 0; i < this.inventory[2].key; i++)
+		{
+			if(soldiers > 0)
+			{
+				person = new Person("Person" + (i+1), "Soldier",80,false,false);
+				soldiers--;
+			}
+			else if(farmers > 0)
+			{
+				person = new Person("Person" + (i+1), "Farmer",80,false,false);
+				farmers--;
+			}
+			else if(workers > 0)
+			{
+				person = new Person("Person" + (i+1), "Worker",80,false,false);
+				workers--;
+			}
+			else if(builders > 0)
+			{
+				person = new Person("Person" + (i+1), "Builder",80,false,false);
+				builders--;
+			}
+			else if(freeman > 0)
+			{
+				person = new Person("Person" + (i+1), "Free man",80,false,false);
+				freeman--;
+			}
+			this.population_list.push(person);
+		}
+		this.widgets[10] = new List(50,350,300,400,"",this.population_list,this.fonts[0]);
+	}
 }
--- server.js
+++ server.js
@@ -40,11 +40,101 @@ function newConnection(socket)
 	
 	function battle(inventory)
 	{
-		//const oldinventory = inventory;
+		//const oldinventory = inventory; !!! newinventory talán???
 		var enemy = inventory[1].key;
+		var population = inventory[2].key;
+		var soldiers = inventory[4].key;
+		var farmers = inventory[6].key;
+		var workers = inventory[8].key;
+		var builders = inventory[10].key;
+		var freeman = inventory[12].key;
 		
-		inventory[4].key -= enemy;
+		var food = inventory[13].key;
+		var resource = inventory[15].key;
+		var happiness = inventory[17].key;
+		
+		// katonák halála 50%-50%
+		for(var i = 0; i < enemy; i++)
+		{
+			if(Math.floor(Math.random() * 101) % 2 == 0)
+			{
+				if(soldiers > 0)
+				{
+					soldiers--;
+				}
+				else if(farmers > 0)
+				{
+					farmers--;
+				}
+				else if(workers > 0)
+				{
+					workers--;
+				}
+				else if(builders > 0)
+				{
+					builders--;
+				}
+				else if(freeman > 0)
+				{
+					freeman--;
+				}
+				else
+				{
+					// vesztett a tag!
+				}
+				population--;
+				happiness-=population;
+			}
+		}
+		
+		// random populáció növekedés
+		
+		if(Math.floor(Math.random() * 101) % 4 == 0)
+		{
+			var mennyi = Math.floor(Math.random() * 4);
+			population+=mennyi;
+			freeman+=mennyi;
+			happiness+=mennyi;
+		}
+		
+		// kaja
+		
+		food+=farmers*2;
+		
+		food -= population;
+		
+		if(food < 0)
+		{
+			happiness-=50;
+		}
+		
+		// sok kaja --> happy
+		else if(food > population*2)
+		{
+			happiness+=population;
+		}
+		
+		// vice versa
+		else if(food < population/2)
+		{
+			happiness-=population;
+		}
+		
+		// nyersanyag
+		
+		resource+=workers;
+		
 		inventory[1].key++;
+		inventory[2].key = population;
+		inventory[4].key = soldiers;
+		inventory[6].key = farmers;
+		inventory[8].key = workers;
+		inventory[10].key = builders;
+		inventory[12].key = freeman;
+		
+		inventory[13].key = food;
+		inventory[15].key = resource;
+		inventory[17].key = happiness;
 		socket.emit('battle',inventory);
 	}
 	
